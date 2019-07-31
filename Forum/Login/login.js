62ff6733968f6e86eb00bceef3b7a4125d5cafee
Joshua Bello <52415643+jbello2550@users.noreply.github.com>
2019-07-31 14:46:30 -0400

    Add files via upload

---
+++ Forum/Login/login.js
@@ -0,0 +1,12 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyAH0-dtYwDZ1_XO8ZwAE7HEAfR2AO-_XY8",
+    authDomain: "potentia-85501.firebaseapp.com",
+    databaseURL: "https://potentia-85501.firebaseio.com",
+    projectId: "potentia-85501",
+    storageBucket: "",
+    messagingSenderId: "615187511736",
+    appId: "1:615187511736:web:3c21267a349c677d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
