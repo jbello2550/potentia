a21744d4a2f82878f47a933cb088e3c110ff08a2
mdabney1 <52415589+mdabney1@users.noreply.github.com>
2019-08-01 22:47:00 -0400

    Update login.js

--- Forum/Login/login.js
+++ Forum/Login/login.js
@@ -1,12 +1,56 @@
- var firebaseConfig = {
-    apiKey: "AIzaSyAH0-dtYwDZ1_XO8ZwAE7HEAfR2AO-_XY8",
-    authDomain: "potentia-85501.firebaseapp.com",
-    databaseURL: "https://potentia-85501.firebaseio.com",
-    projectId: "potentia-85501",
-    storageBucket: "",
-    messagingSenderId: "615187511736",
-    appId: "1:615187511736:web:3c21267a349c677d"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+//start of code
 
+$('#login').submit(function (e){
+  e.preventDefault();
+ 
+ var email = document.getElementById("eusername").value;
+  var password = document.getElementById("password").value;
+
+  console.log("Email", email, "Password", password)
+
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+    // User is signed in.
+    } else {
+    // No user is signed in.
+    }
+  });
+
+    var user = firebase.auth().currentUser;
+    
+  if (user != null) {
+    uid = user.uid;
+  } else {
+  // No user is signed in.
+  }
+
+
+  firebase.auth().signOut().then(function() {
+  // Sign-out successful.
+  }).catch(function(error) {
+  // An error happened.
+  });
+
+});
+
+
+$('#signup').submit(function(e){
+  e.preventDefault();
+
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("passwords").value;
+ firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+  document.getElementById("email").value = "";
+  document.getElementById("passwords").value = "";
+});
