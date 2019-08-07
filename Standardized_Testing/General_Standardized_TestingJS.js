994aabc1e6171519e8ac2c3a3b79615ed2c3365d
Joshua Bello <52415643+jbello2550@users.noreply.github.com>
2019-08-07 10:55:30 -0400

    Add files via upload

--- Standardized_Testing/General_Standardized_TestingJS.js
+++ Standardized_Testing/General_Standardized_TestingJS.js
@@ -1,10 +1,10 @@
 // JavaScript source code for the Standardized_Testing
 var pages = [
-    "/C:/Users/joshu/OneDrive/SAMS/Potentia_Webpage/Standardized_Testing/SAT/SAT.html",
-    "/C:/Users/joshu/OneDrive/SAMS/Potentia_Webpage/Standardized_Testing/ACT/ACT.html",
-    "/C:/Users/joshu/OneDrive/SAMS/Potentia_Webpage/Standardized_Testing/Standardized_Testing.html",
-    "/C:/Users/joshu/OneDrive/SAMS/Potentia_Webpage/Standardized_Testing/AP_Exams/AP_Exams.html",
-    "/C:/Users/joshu/OneDrive/SAMS/Potentia_Webpage/Standardized_Testing/Subject_Test/Subject_Test.html" 
+    "/potentia/Standardized_Testing/SAT/SAT.html",
+    "/potentia/Standardized_Testing/ACT/ACT.html",
+    "/potentia/Standardized_Testing/Standardized_Testing.html",
+    "/potentia/Standardized_Testing/AP_Exams/AP_Exams.html",
+    "/potentia/Standardized_Testing/Subject_Test/Subject_Test.html" 
 ]
 
 
