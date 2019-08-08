3c053ca70d5e9a3fff89e71d318f9d1f840b4894
mdabney1 <52415589+mdabney1@users.noreply.github.com>
2019-08-08 16:01:06 -0400

    Update forum.js

--- Forum/forum.js
+++ Forum/forum.js
@@ -1,15 +1,15 @@
 
-        var firebaseConfig = {
-            apiKey: "AIzaSyAH0-dtYwDZ1_XO8ZwAE7HEAfR2AO-_XY8",
-            authDomain: "potentia-85501.firebaseapp.com",
-            databaseURL: "https://potentia-85501.firebaseio.com",
-            projectId: "potentia-85501",
-            storageBucket: "",
-            messagingSenderId: "615187511736",
-            appId: "1:615187511736:web:3c21267a349c677d"
-        };
-        // Initialize Firebase
-        firebase.initializeApp(firebaseConfig);
+ var firebaseConfig = {
+     apiKey: "AIzaSyAH0-dtYwDZ1_XO8ZwAE7HEAfR2AO-_XY8",
+     authDomain: "potentia-85501.firebaseapp.com",
+     databaseURL: "https://potentia-85501.firebaseio.com",
+     projectId: "potentia-85501",
+     storageBucket: "",
+     messagingSenderId: "615187511736",
+     appId: "1:615187511736:web:3c21267a349c677d"
+    };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 var db = firebase.firestore();
 
